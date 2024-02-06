00577bc3da380809dd234301abd0833a2375597d
VictorGM01 <victormarques8801@gmail.com>
2024-02-06 13:51:16 -0300

    feat: implementa busca de estoque por id

--- src/routes/EstoqueRoutes.js
+++ src/routes/EstoqueRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 import cadastroEstoque from '../interfaces/controllers/CadastroEstoqueController.js';
+import consultaEstoques from '../interfaces/controllers/ConsultaEstoquesController.js';
 
 
 router
-  .post('/cadastro', cadastroEstoque);
+  .post('/cadastro', cadastroEstoque)
+  .get('/:id', consultaEstoques);
 
 
 export default router;
