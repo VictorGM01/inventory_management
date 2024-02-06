27f62aea2963f577d7e55bd5a407f26d9c120e24
VictorGM01 <victormarques8801@gmail.com>
2024-02-06 12:59:42 -0300

    fora de contexto: adiciona novo campo para conflito

--- src/entities/Produto.js
+++ src/entities/Produto.js
@@ -1,11 +1,12 @@
-class Produtos {
-  constructor(id, nome, preco, quantidade, estoque_id) {
+class Produto {
+  constructor(id, nome, preco, quantidade, estoque_id, categoria) { // Adicionando um novo campo "categoria"
     this.id = id;
     this.nome = nome;
     this.preco = preco;
     this.quantidade = quantidade;
     this.estoque_id = estoque_id;
+    this.categoria = categoria; // Novo campo
   }
 }
 
-export default Produtos;
+export default Produto;
