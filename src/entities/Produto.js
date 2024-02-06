407bdb4afee553d4630a7bf518040cdb6bd3fb06
VictorGM01 <victormarques8801@gmail.com>
2024-02-06 13:00:35 -0300

    fora de contexto: alterações para conflito com a develop

--- src/entities/Produto.js
+++ src/entities/Produto.js
@@ -1,11 +1,11 @@
-class Produtos {
-  constructor(id, nome, preco, quantidade, estoque_id) {
+class Produto {
+  constructor(id, nome, preco, desconto, quantidade, estoque_id) { // Alterando "preco" para incluir "desconto"
     this.id = id;
     this.nome = nome;
-    this.preco = preco;
+    this.preco = preco - desconto; // Modificado para aplicar desconto diretamente
     this.quantidade = quantidade;
     this.estoque_id = estoque_id;
   }
 }
 
-export default Produtos;
+export default Produto;
