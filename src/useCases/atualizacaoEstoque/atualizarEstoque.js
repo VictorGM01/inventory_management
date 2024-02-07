d915da70a1b468cadecb089d6071ebd0e351d745
VictorGM01 <victormarques8801@gmail.com>
2024-02-07 10:26:31 -0300

    feat: adiciona caso de uso para atualizar estoque

---
+++ src/useCases/atualizacaoEstoque/atualizarEstoque.js
@@ -0,0 +1,13 @@
+// import Produto from '../../entities/Produto.js.js';
+
+class AtualizarEstoque {
+  constructor(estoqueRepository) {
+    this.estoqueRepository = estoqueRepository;
+  }
+
+  async execute(id, quantidade) {
+    return("Ainda não implmentado!")
+  }
+}
+
+export default AtualizarEstoque;
